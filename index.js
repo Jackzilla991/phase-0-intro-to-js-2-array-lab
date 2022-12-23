387e7217d190d888567ca16cbdd63c37ecfa3d53
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-22 20:59:02 -0500

    commit tree eb57869bde9513a3dd0b2b08c4fdd69647988d7d

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution here!
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Array functions
+
+const destructivelyAppendCat = (name) => {
+  cats.push(name);
+  return cats;
+}
+
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+  return cats;
+}
+
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+  return cats;
+}
+
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+  return cats;
+}
+
+const appendCat = (name) => {
+  let newCats = [...cats];
+  newCats.push(name);
+  return newCats;
+}
+
+const prependCat = (name) => {
+  let newCats = [...cats];
+  newCats.unshift(name);
+  return newCats;
+}
+
+const removeLastCat = () => {
+  let newCats = [...cats];
+  newCats.pop();
+  return newCats;
+}
+
+const removeFirstCat = () => {
+  let newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
+
